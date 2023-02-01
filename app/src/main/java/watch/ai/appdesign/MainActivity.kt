ad5e74dbd2150c134ae4a465192538df7ed7b72a
Thinh Lam <78235992+tlam89@users.noreply.github.com>
2023-02-01 11:04:54 -0500

    add activities

---
+++ app/src/main/java/watch/ai/appdesign/MainActivity.kt
@@ -0,0 +1,24 @@
+package watch.ai.appdesign
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val confirm = findViewById<Button>(R.id.ConfirmButton);
+        confirm.setOnClickListener {
+            val Intent = Intent(this, NewActivity1::class.java)
+            startActivity(Intent)
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/watch/ai/appdesign/NewActivity1.kt
@@ -0,0 +1,11 @@
+package watch.ai.appdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new1)
+    }
+}
